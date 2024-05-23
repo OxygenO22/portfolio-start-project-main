d4e106cf7ca88f30364cd35fcecbc34344ced68d
OxygenO22 <bamaik@mail.ru>
2024-05-23 17:54:18 +0300

    Added some information in project cards

--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -1,12 +1,18 @@
 // social links
 
-const Linkedin = "https://www.linkedin.com/in/alexandr-budzko-823443218/";
-const GitHub = "https://github.com/OxygenO22";
-const Facebook = "https://facebook.com/sashabudzko";
+export const Linkedin = "https://www.linkedin.com/in/alexandr-budzko-823443218/";
+export const GitHub = "https://github.com/OxygenO22";
+export const Facebook = "https://facebook.com/sashabudzko";
 
 // projects links
 
- const LessonerLink = "https://the-lessoner-com-1n7o.onrender.com/";
- const AllAboutAllLink = "https://oxygeno22.github.io/aaa/";
-
-export {Linkedin, GitHub, Facebook, LessonerLink, AllAboutAllLink}
+ export const LessonerLink = "https://the-lessoner-com-1n7o.onrender.com/";
+ export const AllAboutAllLink = "https://oxygeno22.github.io/aaa/";
+ export const AllAboutAllCode = "https://github.com/OxygenO22/aaa";
+ export const CitiesGameLink = "https://oxygeno22.github.io/Cities_Game/";
+ export const CitiesGameCode = "https://github.com/OxygenO22/Cities_Game";
+ export const ShotThemAllLink = "https://oxygeno22.github.io/ShotThemAll/";
+ export const ShotThemAllCode = "https://github.com/OxygenO22/Docker-ShotThemAll";
+ export const ElegantLink = "https://oxygeno22.github.io/Elegant/about.html";
+ export const ElegantCode = "https://github.com/OxygenO22/Elegant";
+ export const StoreFinderLink = "https://github.com/OxygenO22/Docker-StoresFinder";
--- src/layout/sections/projects/ProjectCardData.ts
+++ src/layout/sections/projects/ProjectCardData.ts
@@ -4,7 +4,7 @@ import ProjectPicture3 from '../../../assets/images/pictures/projects/proj-3.web
 import ProjectPicture4 from '../../../assets/images/pictures/projects/proj-4.webp';
 import ProjectPicture5 from '../../../assets/images/pictures/projects/proj-5.webp';
 import ProjectPicture6 from '../../../assets/images/pictures/projects/proj-6.webp';
-import { AllAboutAllLink, LessonerLink } from '../../../constants/Constants';
+import { AllAboutAllCode, AllAboutAllLink, CitiesGameCode, CitiesGameLink, ElegantCode, ElegantLink, LessonerLink, ShotThemAllCode, ShotThemAllLink,  StoreFinderLink } from '../../../constants/Constants';
 
 export const ProjectCardData = [
   {
@@ -18,7 +18,7 @@ export const ProjectCardData = [
     chainLinkText: 'Live Preview', 
     githubLinkText: 'View Code', 
     chainLink: `${LessonerLink}`, 
-    githubLink: '#'
+    githubLink: `${LessonerLink}`
   
   },
   {
@@ -32,58 +32,58 @@ export const ProjectCardData = [
     chainLinkText: 'Live Preview', 
     githubLinkText: 'View Code', 
     chainLink: `${AllAboutAllLink}`, 
-    githubLink: '#'
+    githubLink: `${AllAboutAllCode}`
   },
   {
     id: 3, 
     src: `${ProjectPicture3}`, 
     alt: 'Project Picture 3', 
-    title: 'Project Tile goes here', 
-    mainText: 'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content', 
+    title: 'Cities Game', 
+    mainText: 'This is a “Cities” game. The application saves settings in LocalStorage (current progress, sound on/off, keyboard open/closed, which city base the game was played with). The application also receives information about the selected city (description and picture) from a third-party site, and it brings the information to the desired form (finds the necessary classes in the markup, removes unnecessary information, and does not get a preview image, but one with good resolution).', 
     stackSpanText: 'Tech stack :', 
-    stackText: 'HTML , JavaScript, SASS, React', 
+    stackText: 'HTML, CSS, SCSS, JavaScript, Docker, Bitbucket, LocalStorage', 
     chainLinkText: 'Live Preview', 
     githubLinkText: 'View Code', 
-    chainLink: '#', 
-    githubLink: '#'
+    chainLink: `${CitiesGameLink}`, 
+    githubLink: `${CitiesGameCode}`
   },
   {
     id: 4, 
     src: `${ProjectPicture4}`, 
     alt: 'Project Picture 4', 
-    title: 'Project Tile goes here', 
-    mainText: 'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content', 
+    title: 'Shot Them All', 
+    mainText: 'This is a shooter game.', 
     stackSpanText: 'Tech stack :', 
-    stackText: 'HTML , JavaScript, SASS, React', 
+    stackText: 'HTML, CSS, SCSS, JavaScript, Canvas, LocalStorage', 
     chainLinkText: 'Live Preview', 
     githubLinkText: 'View Code', 
-    chainLink: '#', 
-    githubLink: '#'
+    chainLink: `${ShotThemAllLink}`, 
+    githubLink: `${ShotThemAllCode}`
   },
   {
     id: 5, 
     src: `${ProjectPicture5}`, 
     alt: 'Project Picture 5', 
-    title: 'Project Tile goes here', 
-    mainText: 'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content', 
+    title: 'ELEGANT', 
+    mainText: 'This is a multi-page site.', 
     stackSpanText: 'Tech stack :', 
-    stackText: 'HTML , JavaScript, SASS, React', 
+    stackText: 'HTML, CSS, JavaScript, Adobe Photoshop, Bootstrap5, Flexbox, Grid, @ keyframes', 
     chainLinkText: 'Live Preview', 
     githubLinkText: 'View Code', 
-    chainLink: '#', 
-    githubLink: '#'
+    chainLink: `${ElegantLink}`, 
+    githubLink: `${ElegantCode}`
   },
   {
     id: 6, 
     src: `${ProjectPicture6}`, 
     alt: 'Project Picture 6', 
-    title: 'Project Tile goes here', 
-    mainText: 'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content', 
+    title: 'StoreFinder', 
+    mainText: 'This is a one-page application that works with a database of stores and their products. he application receives data about stores and their products from a local server. The application allows you to: search by stores and products, sort by product status, sort by columns of the product table, sorting products by status, by table columns and search work together taking into account all parameters, create/delete stores and products, edit products.', 
     stackSpanText: 'Tech stack :', 
-    stackText: 'HTML , JavaScript, SASS, React', 
+    stackText: 'HTML, CSS, JavaScript, MVC, Fetch, JSDoc, local server', 
     chainLinkText: 'Live Preview', 
     githubLinkText: 'View Code', 
-    chainLink: '#', 
-    githubLink: '#'
+    chainLink: `${StoreFinderLink}`, 
+    githubLink: `${StoreFinderLink}`
   },
 ]
